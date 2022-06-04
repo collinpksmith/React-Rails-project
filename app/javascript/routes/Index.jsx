fd8db3d742ebf78579699be95c6279a6931a7ef4
SeniorEng <ademiagood@gmail.com>
2022-06-04 18:05:06 +0000

    Add Recipe page

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "../components/Home"
 import Recipes from "../components/Recipes"
+import Recipe from "../components/Recipe"
 
 const routes = () => {
   return (
@@ -9,6 +10,7 @@ const routes = () => {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/recipes" exact element={<Recipes />} />
+        <Route path="/recipe/:id" exact element={<Recipe />} />
       </Routes>
     </Router>
   )
